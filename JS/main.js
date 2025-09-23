4d53dbe71292b7e3f50727dbf468b235072f835b
Besho <Besho>
2025-09-23 14:58:41 +0300

    .

--- JS/main.js
+++ JS/main.js
@@ -19,3 +19,48 @@ document.getElementById('menu-btn').addEventListener('click', (e)=>{
 document.getElementById('x-mark').addEventListener('click', (e)=>{
     toggleMobileMenu();
 });
+
+let carosalCounter =  0;
+
+const carosalImages = [
+    './img/banner_home1.png',
+    './img/banner_home2.png'
+];
+
+function carosalSlideRight(){
+    if(carosalCounter == carosalImages.length - 1){
+        carosalCounter = 0;
+        return carosalImages[carosalCounter];
+    }
+
+    carosalCounter += 1;
+    return carosalImages[carosalCounter];
+}
+
+function carosalSlideLeft(){
+    if(carosalCounter == 0){
+        carosalCounter = carosalImages.length - 1;
+        return carosalImages[carosalCounter];
+    }
+
+    carosalCounter -= 1;
+    return carosalImages[carosalCounter];
+}
+
+
+function changeImage(newSrc) {
+    carosalCurrentImage.classList.add("fade-out");
+
+    setTimeout(() => {
+        carosalCurrentImage.src = newSrc;
+        carosalCurrentImage.classList.remove("fade-out"); 
+    }, 300);
+}
+
+document.getElementById('prevImg').addEventListener('click', () => {
+    changeImage(carosalSlideLeft());
+});
+
+document.getElementById('nxtImg').addEventListener('click', () => {
+    changeImage(carosalSlideRight());
+});
